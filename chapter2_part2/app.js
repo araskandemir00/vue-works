184a11f35d1d86600a27f582418873b11a4f661a
Hasan Aras Kandemir <201466@GM.ISBANK.COM.TR>
2022-09-22 15:30:55 +0300

    chapter2_part2

--- chapter2_part2/app.js
+++ chapter2_part2/app.js
@@ -1,21 +1,30 @@
 const app = Vue.createApp({
     data() {
       return {
-        counter: 0,
-        name: ''
+        counter: 10,
+        addMuch: 5,
+        reduceMuch: 5,
+        name: '',
+        confirmedName: '',
+        surname: 'Kandemir'
       };
     },
-    methods: {
-      setName(event, lastName) {
-        this.name = event.target.value + ' ' + lastName;
-      },
-      add(num) {
-        this.counter = this.counter + num;
-      },
-      reduce(num) {
-        this.counter = this.counter - num;
-        // this.counter--;
-      }
+    methods:{
+        confirmInput() {
+            this.confirmedName = this.name;
+        },
+        submitForm() {
+            alert('Submitted!');
+        },
+        setName(event, lastName) {
+            this.name = event.target.value + ' ' + lastName; 
+        },
+        add(n) {
+            this.counter = this.counter +  n;
+        },
+        reduce(n) {
+            this.counter = this.counter - n;
+        }
     }
   });
   
