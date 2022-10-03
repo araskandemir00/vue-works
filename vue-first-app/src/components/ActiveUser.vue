c7be816efbd5551bea0a68a64f3eb90a9d8c2965
Hasan Aras Kandemir <201466@GM.ISBANK.COM.TR>
2022-10-03 11:27:40 +0300

    hw6

---
+++ vue-first-app/src/components/ActiveUser.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <h2>{{ username }}</h2>
+        <h2>{{ userage }}</h2>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: {
+            username: {
+                type: String,
+                required: true
+            },
+            userage: {
+                type: Number,
+                required: true
+            },
+        }
+    }
+</script>
---
+++ vue-first-app/src/components/UserData.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData">
+            <div>
+                <input type="text" v-model="enteredName"/>
+                <input type="text" v-model="enteredAge"/>
+            </div>
+            <button @click="submitData">Change User Data</button>
+        </form>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                enteredName: '',
+                enteredAge: '',
+            };
+        },
+        methods: {
+            submitData() {
+                this.$$emit('set-data', this.enteredName, this.enteredAge);
+            }
+        }
+    }
+</script>
